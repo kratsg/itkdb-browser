78a58855c32faedfb5d58eff5dbe378bde2f66ee
Giordon Stark <kratsg@gmail.com>
2023-01-19 16:42:04 -0800

    initial version for printing institution information

--- src/itkdb_browser/tui.py
+++ src/itkdb_browser/tui.py
@@ -1,52 +1,100 @@
 from __future__ import annotations
 
+from operator import itemgetter
 from typing import Any
 
-from rich.markdown import Markdown
+import itkdb
+from rich.pretty import Pretty
 from textual.app import App, ComposeResult
-from textual.containers import Container
-from textual.widgets import Button, Static
+from textual.containers import Horizontal, Vertical
+from textual.reactive import reactive
+from textual.widgets import Footer, Header, Label, ListItem, ListView, Static
 
-WELCOME_MD = """\
-# Welcome!
 
-This is the itkdb-browser.
-"""
+class InstitutionItem(ListItem):
+    """An Institution ListItem."""
+
+    __slots__ = ("value",)
+
+    def __init__(self, institution: dict[str, Any]):
+        super().__init__(Label(institution["name"]))
+        self.value = institution
+
+
+class InstitutionList(ListView):
+    """A widget to display a list of institutions."""
+
+    institutions: reactive[list[dict[str, Any]]] = reactive([])
+
+    def __init__(self, client: itkdb.Client, classes: str = ""):
+        self.client = client
+        super().__init__(classes=classes)
+
+    def load_institutions(self) -> None:
+        """Retrieve institutions from API."""
+        self.institutions = list(self.client.get("listInstitutions"))
+
+    def on_mount(self) -> None:
+        """Call after mounting."""
+        self.load_institutions()
+
+    def watch_institutions(self) -> None:
+        """Called when the institutions attribute changes."""
+        self.clear()
+        for institution in sorted(self.institutions, key=itemgetter("name")):
+            self.append(InstitutionItem(institution))
+
+
+class InstitutionDisplay(Static):
+    """A widget to display institution details."""
+
+    institution: reactive[dict[str, Any]] = reactive({})
+
+    def watch_institution(self) -> None:
+        """Called when the institution attribute changes."""
+        self.update(Pretty(self.institution))
 
 
 class Browser(App[Any]):
     """A basic implementation of the itkdb-browser TUI"""
 
+    # BINDINGS = [("q", "exit", "Quit"), ("d", "toggle_dark", "Toggle dark mode")]
+
     DEFAULT_CSS = """
-        Browser {
-            width: 100%;
-            height: 100%;
-            background: $surface;
-        }
-        Browser Container {
-            padding: 1;
-            background: $panel;
-            color: $text;
-        }
-        Browser #text {
-            margin:  0 1;
-        }
-        Browser #close {
-            dock: bottom;
-            width: 100%;
-        }
+    Browser > Static {
+        content-align: center middle;
+        background: crimson;
+        border: solid darkred;
+        height: 1fr;
+    }
+
+    .column {
+        width: 1fr;
+    }
     """
 
-    def on_mount(self) -> None:
-        """Call after terminal goes in to application mode"""
-        self.screen.styles.background = "darkblue"
-        self.screen.styles.border = ("heavy", "white")
+    def __init__(self) -> None:
+        super().__init__()
+        self.institution_list = InstitutionList(itkdb.Client(), classes="column")
+        self.institution_details = InstitutionDisplay()
 
-    def on_button_pressed(self) -> None:
-        """Call when a button is pressed"""
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.dark = not self.dark
+
+    def action_exit(self) -> None:
+        """An action to exit."""
         self.exit()
+
+    def on_list_view_selected(self, message: ListView.Selected) -> None:
+        """When institution has been chosen."""
+        self.institution_details.institution = getattr(message.item, "value", {})
 
     def compose(self) -> ComposeResult:
         """Call to compose the app"""
-        yield Container(Static(Markdown(WELCOME_MD), id="text"), id="md")
-        yield Button("OK", id="close", variant="success")
+        yield Header()
+        yield Footer()
+        yield Horizontal(
+            self.institution_list, Vertical(self.institution_details, classes="column")
+        )
+        self.set_focus(self.institution_list)
