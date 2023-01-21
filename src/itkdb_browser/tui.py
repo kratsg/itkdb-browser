f4a771d87aa28d001569ebc89e77c6ff7732d3b0
Giordon Stark <kratsg@gmail.com>
2023-01-21 10:49:29 -0800

    flesh out app with a login screen

--- src/itkdb_browser/tui.py
+++ src/itkdb_browser/tui.py
@@ -6,9 +6,85 @@ from typing import Any
 import itkdb
 from rich.pretty import Pretty
 from textual.app import App, ComposeResult
-from textual.containers import Horizontal, Vertical
+from textual.containers import Container, Horizontal, Vertical
 from textual.reactive import reactive
-from textual.widgets import Footer, Header, Label, ListItem, ListView, Static
+from textual.screen import Screen
+from textual.widgets import (
+    Button,
+    Footer,
+    Header,
+    Input,
+    Label,
+    ListItem,
+    ListView,
+    Static,
+    TextLog,
+)
+
+
+class LoginScreen(Screen):
+
+    accessCode1 = reactive(itkdb.settings.ITKDB_ACCESS_CODE1)
+    accessCode2 = reactive(itkdb.settings.ITKDB_ACCESS_CODE2)
+
+    def login(self) -> None:
+        """Called to perform login."""
+        try:
+            user = itkdb.core.User(
+                accessCode1=self.accessCode1, accessCode2=self.accessCode2
+            )
+            user.authenticate()
+            self.app.client = itkdb.Client(user=user)  # type: ignore[attr-defined]
+            self.app.login()  # type: ignore[attr-defined]
+        except itkdb.exceptions.ResponseException as exc:
+            self.app.bell()
+            self.query_one("TextLog").write(str(exc))  # type: ignore[attr-defined]
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Event handler called when login button is pressed."""
+        button_id = event.button.id
+        if button_id == "login":
+            self.login()
+            event.stop()
+
+    def on_input_changed(self, event: Input.Changed) -> None:
+        if event.input.id == "accessCode1":
+            self.accessCode1 = event.value
+            event.stop()
+        elif event.input.id == "accessCode2":
+            self.accessCode2 = event.value
+            event.stop()
+        else:
+            pass
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+        yield Container(
+            Horizontal(
+                Static("Access Code 1", classes="labels"),
+                Input(
+                    self.accessCode1,
+                    placeholder="code",
+                    id="accessCode1",
+                    classes="access_codes",
+                ),
+                classes="input_row",
+            ),
+            Horizontal(
+                Static("Access Code 2", classes="labels"),
+                Input(
+                    self.accessCode2,
+                    placeholder="code",
+                    id="accessCode2",
+                    classes="access_codes",
+                ),
+                classes="input_row",
+            ),
+            Button("Login", id="login", variant="primary"),
+            id="dialog",
+        )
+        yield Horizontal(TextLog(), id="textlog")
 
 
 class InstitutionItem(ListItem):
@@ -24,24 +100,13 @@ class InstitutionItem(ListItem):
 class InstitutionList(ListView):
     """A widget to display a list of institutions."""
 
-    institutions: reactive[list[dict[str, Any]]] = reactive([])
-
-    def __init__(self, client: itkdb.Client, classes: str = ""):
-        self.client = client
-        super().__init__(classes=classes)
-
     def load_institutions(self) -> None:
-        """Retrieve institutions from API."""
-        self.institutions = list(self.client.get("listInstitutions"))
-
-    def on_mount(self) -> None:
-        """Call after mounting."""
-        self.load_institutions()
-
-    def watch_institutions(self) -> None:
-        """Called when the institutions attribute changes."""
         self.clear()
-        for institution in sorted(self.institutions, key=itemgetter("name")):
+        institutions = self.app.client.get("listInstitutions")  # type: ignore[attr-defined]
+        for institution in sorted(
+            list(institutions),
+            key=itemgetter("name"),
+        ):
             self.append(InstitutionItem(institution))
 
 
@@ -60,23 +125,15 @@ class Browser(App[Any]):
 
     BINDINGS = [("q", "exit", "Quit"), ("d", "toggle_dark", "Toggle dark mode")]
 
-    DEFAULT_CSS = """
-    Browser > Static {
-        content-align: center middle;
-        background: crimson;
-        border: solid darkred;
-        height: 1fr;
-    }
+    SCREENS = {"login": LoginScreen()}
 
-    .column {
-        width: 1fr;
-    }
-    """
+    CSS_PATH = "tui.css"
 
     def __init__(self) -> None:
         super().__init__()
-        self.institution_list = InstitutionList(itkdb.Client(), classes="column")
+        self.institution_list = InstitutionList(classes="column")
         self.institution_details = InstitutionDisplay()
+        self.client = None
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
@@ -90,6 +147,15 @@ class Browser(App[Any]):
         """When institution has been chosen."""
         self.institution_details.institution = getattr(message.item, "value", {})
 
+    def login(self) -> None:
+        """Called when the LoginScreen has logged in."""
+        self.pop_screen()
+        self.institution_list.load_institutions()
+
+    def on_mount(self) -> None:
+        """Call after entering application mode."""
+        self.push_screen("login")
+
     def compose(self) -> ComposeResult:
         """Call to compose the app"""
         yield Header()
@@ -97,4 +163,3 @@ class Browser(App[Any]):
         yield Horizontal(
             self.institution_list, Vertical(self.institution_details, classes="column")
         )
-        self.set_focus(self.institution_list)
