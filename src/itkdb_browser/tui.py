d85d272204aaf9ba3d59c4bb7289f52ebc129056
Giordon Stark <kratsg@gmail.com>
2023-01-24 20:32:06 -0800

    add mainscreen to show user information after login

--- src/itkdb_browser/tui.py
+++ src/itkdb_browser/tui.py
@@ -4,7 +4,9 @@ from operator import itemgetter
 from typing import Any
 
 import itkdb
+from rich.console import RenderableType
 from rich.pretty import Pretty
+from rich.progress import BarColumn, Progress
 from textual.app import App, ComposeResult
 from textual.containers import Container, Horizontal, Vertical
 from textual.reactive import reactive
@@ -22,6 +24,25 @@ from textual.widgets import (
 )
 
 
+class IntervalUpdater(Static):
+    _renderable_object: RenderableType
+
+    def __init__(self, renderable_object: RenderableType) -> None:
+        super().__init__(renderable_object)
+
+    def on_mount(self) -> None:
+        self.interval_update = self.set_interval(1 / 60, self.refresh)
+
+
+class IndeterminateProgressBar(IntervalUpdater):
+    """Basic indeterminate progress bar widget based on rich.progress.Progress."""
+
+    def __init__(self) -> None:
+        self._renderable_object = Progress(BarColumn(bar_width=None))
+        self._renderable_object.add_task("", total=None)
+        super().__init__(self._renderable_object)
+
+
 class LoginScreen(Screen):
     """Screen for logging user in."""
 
@@ -35,11 +56,11 @@ class LoginScreen(Screen):
                 accessCode1=self.access_code1, accessCode2=self.access_code2
             )
             user.authenticate()
-            self.app.client = itkdb.Client(user=user)  # type: ignore[attr-defined]
-            self.app.login()  # type: ignore[attr-defined]
+            self.app.client = itkdb.Client(user=user)
+            self.app.login()
         except itkdb.exceptions.ResponseException as exc:
             self.app.bell()
-            self.query_one("TextLog").write(str(exc))  # type: ignore[attr-defined]
+            self.query_one("TextLog").write(str(exc))
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         """Event handler called when login button is pressed."""
@@ -89,6 +110,69 @@ class LoginScreen(Screen):
         yield Horizontal(TextLog(), id="textlog")
 
 
+class UserDetails(Static):
+    """Widget for displaying user information."""
+
+    def on_mount(self) -> None:
+        email = self.app.user["email"]
+        text = f"""[b]{self.app.user['firstName']} {self.app.user['lastName']}[/b] [link=mailto:{email} lightblue]({email})[/link]
+[b]Identity:[/b] {self.app.client.user.identity}"""
+        preferences = self.app.user.get("preferences", {})
+        if preferences:
+            text += """\n[u yellow]Preferences:[/u yellow]"""
+            for pref, value in preferences.items():
+                text += f"\n  {pref}: {value}"
+        self.update(text)
+
+
+class UserInstitutionDetails(Static):
+    """Widget for displaying institution information."""
+
+    def __init__(self, institution: dict[str, Any], **kwargs: Any):
+        super().__init__("institution details", **kwargs)
+        self._id = institution["id"]
+        self._code = institution["code"]
+        self._name = institution["name"]
+        self._roles = {}
+        for role in [
+            "default",
+            "executive",
+            "authority",
+            "componentManager",
+            "clusterManager",
+        ]:
+            self._roles[role] = bool(institution.get(role))
+        self._notifications = {}
+        for notification in ["shipping", "tests", "components"]:
+            self._notifications[notification] = bool(
+                (institution.get("notification") or {}).get(notification)
+            )
+
+    def _render_bool_str(self, value: bool) -> str:
+        return ":white_check_mark:" if value else ":cross_mark:"
+
+    def on_mount(self) -> None:
+        text = f"[b]{self._name} [gray]({self._code})[/gray][/b]"
+        text += "\n[u yellow]Roles:[/u yellow]"
+        for role, value in self._roles.items():
+            text += f"\n  {self._render_bool_str(value)} {role}"
+        text += "\n[u yellow]Notification:[/u yellow]"
+        for notify, value in self._notifications.items():
+            text += f"\n  {self._render_bool_str(value)} {notify}"
+        self.update(text)
+
+
+class MainScreen(Screen):
+    """Screen for displaying user information."""
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+        yield UserDetails()
+        for institution in self.app.user["institutions"]:
+            yield UserInstitutionDetails(institution)
+
+
 class InstitutionItem(ListItem):
     """An Institution ListItem."""
 
@@ -105,7 +189,7 @@ class InstitutionList(ListView):
     def load_institutions(self) -> None:
         """Load up the institutions in the list view."""
         self.clear()
-        institutions = self.app.client.get("listInstitutions")  # type: ignore[attr-defined]
+        institutions = self.app.client.get("listInstitutions")
         for institution in sorted(
             list(institutions),
             key=itemgetter("name"),
@@ -123,19 +207,39 @@ class InstitutionDisplay(Static):
         self.update(Pretty(self.institution))
 
 
+class InstitutionScreen(Screen):
+    """Screen for displaying institutions."""
+
+    def on_list_view_selected(self, message: ListView.Selected) -> None:
+        """When institution has been chosen."""
+        self.query_one("InstitutionDisplay").institution = getattr(
+            message.item, "value", {}
+        )
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+        yield Horizontal(
+            InstitutionList(classes="column"),
+            Vertical(InstitutionDisplay(), classes="column"),
+        )
+
+
 class Browser(App[Any]):
     """A basic implementation of the itkdb-browser TUI"""
 
     BINDINGS = [("q", "exit", "Quit"), ("d", "toggle_dark", "Toggle dark mode")]
 
-    SCREENS = {"login": LoginScreen()}
+    SCREENS = {
+        "login": LoginScreen(),
+        "main": MainScreen(),
+        "institution": InstitutionScreen(),
+    }
 
     CSS_PATH = "tui.css"
 
     def __init__(self) -> None:
         super().__init__()
-        self.institution_list = InstitutionList(classes="column")
-        self.institution_details = InstitutionDisplay()
         self.client = None
 
     def action_toggle_dark(self) -> None:
@@ -146,14 +250,13 @@ class Browser(App[Any]):
         """An action to exit."""
         self.exit()
 
-    def on_list_view_selected(self, message: ListView.Selected) -> None:
-        """When institution has been chosen."""
-        self.institution_details.institution = getattr(message.item, "value", {})
-
     def login(self) -> None:
         """Called when the LoginScreen has logged in."""
+        self.user = self.client.get(
+            "getUser", json=dict(userIdentity=self.client.user.identity)
+        )
         self.pop_screen()
-        self.institution_list.load_institutions()
+        self.push_screen("main")
 
     def on_mount(self) -> None:
         """Call after entering application mode."""
@@ -163,6 +266,3 @@ class Browser(App[Any]):
         """Call to compose the app"""
         yield Header()
         yield Footer()
-        yield Horizontal(
-            self.institution_list, Vertical(self.institution_details, classes="column")
-        )
